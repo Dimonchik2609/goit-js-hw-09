9290e8fed5cd69c53acdf78d8556d56c170c3e0b
Dmytro <magnit2006@gmail.com>
2025-06-17 02:49:24 +0300

    first commit

---
+++ src/js/2-form.js
@@ -0,0 +1,51 @@
+const storageKey = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+const formData = {
+  email: '',
+  message: '',
+};
+
+function collectValuesToStorageHandler(e) {
+  const name = e.target.name;
+  const value = e.target.value;
+
+  formData[name] = value.trim();
+
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+}
+
+formEl.addEventListener('input', collectValuesToStorageHandler);
+
+const addStorageValueToForm = storageValue => {
+  if (!storageValue) {
+    return;
+  }
+  const { email, message } = formEl.elements;
+  const { email: parsedEmail, message: parsedMessage } =
+    JSON.parse(storageValue);
+
+  email.value = parsedEmail;
+  message.value = parsedMessage;
+
+  formData.email = parsedEmail;
+  formData.message = parsedMessage;
+};
+
+addStorageValueToForm(localStorage.getItem(storageKey));
+
+function formSubmitHandler(e) {
+  e.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    console.log('«Fill please all fields».');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem(storageKey);
+  formData.email = '';
+  formData.message = '';
+  e.currentTarget.reset();
+}
+
+formEl.addEventListener('submit', formSubmitHandler);
